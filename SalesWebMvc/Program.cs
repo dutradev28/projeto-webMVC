8a937b4939d1461bfd3e4685c1d87f36b37f2490
Luis Carlos <luiscarlosdutrajunior23@gmail.com>
2024-07-11 01:09:14 -0300

    Criado Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,22 +1,36 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar DbContext ao contêiner de serviços
+
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext")),
         mysqlOptions => mysqlOptions.MigrationsAssembly("SalesWebMvc")));
 
 
+builder.Services.AddTransient<SeedingService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<SalesWebMvcContext>();
+        var seedingService = services.GetRequiredService<SeedingService>();
+        seedingService.Seed(); 
+    }
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();

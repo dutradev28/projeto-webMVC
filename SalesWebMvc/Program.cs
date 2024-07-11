1c75dc658611280f8ca81fb5934a5334f59ba4b3
Luis Carlos <luiscarlosdutrajunior23@gmail.com>
2024-07-10 23:18:30 -0300

    Adaptação do MySql e primeira migracao

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,20 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionar DbContext ao contêiner de serviços
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext")),
+        mysqlOptions => mysqlOptions.MigrationsAssembly("SalesWebMvc")));
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
